11be4bbd6eaf479025b9be576ae61f63fd135ec5
an-slua-sidhe <polocoinn@yahoo.ie>
2020-04-16 17:20:51 +0100

    Internal hyperlinks to other parts of the page included. List of restaurants added below Google Map.

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,6 +1,6 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 6,
+        zoom: 5.5,
         center: {
             lat: 53.4,
             lng: -7.9
