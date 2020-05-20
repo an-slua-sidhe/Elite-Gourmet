6adcf1c07e5094755a2685ad4e6824b1e4440d73
an-slua-sidhe <polocoinn@yahoo.ie>
2020-05-20 10:36:50 +0100

    Add sweet alert to contact form js

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -7,9 +7,21 @@ function sendMail(contactForm) {
     }).then(
         function (response) {
             console.log("Email Sent!", response);
+            swal({
+                title: "Your Email was Sent!",
+                text: "Thank you for contacting Elite Gourmet. \n We will be in touch with you shortly.",
+                icon: "success",
+                button: "Return to Site",
+              });
         },
         function (error) {
             console.log("Email Not Sent...", error);
+            swal({
+                title: "Ooops!",
+                text: "We're sorry, there was an error! \n Please try again.",
+                icon: "error",
+                button: "Try Again",
+              });
         });
     return false;
 }
